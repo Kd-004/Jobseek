0c89324a7e4c765908b6f7cc93617d7cb88ebd3d
TUF <TUF@ASUS>
2026-07-08 11:37:07 +0530

    chg

---
+++ mainProject/Controllers/DashboardController.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using mainProject.Models;
+using AspNetCoreGeneratedDocument;
+
+namespace mainProject.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ILogger<DashboardController> _logger;
+
+        public DashboardController(ILogger<DashboardController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Landing Page
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Jobs Page
+        public IActionResult Jobs()
+        {
+            return View();
+        }
+
+        // Companies Page
+        public IActionResult Companies()
+        {
+            return View();
+        }
+
+        // About Page
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        // Contact Page
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        // Privacy Page
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        // Error Page
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
+        }
+    }
+}
